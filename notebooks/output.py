31ef0c7d86d430b6022f50f989246198552595b6
Jonathan Reeve <jon.reeve@gmail.com>
2017-02-26 16:55:48 -0500

    remove unnecessary file

--- notebooks/output.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-for i, article in enumerate(data): 
-#     clear_output()
-    print('\r', 'Matching article %s of %s' % (i, len(data)), end='')
-    if 'numMatches' not in article: 
-        articleText = Text(article['text'], article['record_title'], nlp)
-        article['numMatches'], article['Locations in A'], article['Locations in B'] =         Matcher(mm, articleText).match()
